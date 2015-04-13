1616837807989050b458c702b8aa872f364ca2a2
Timo Weiß <info@timo-weiss.com>
2015-04-13 11:44:02 +0200

    update tests

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ var it = lab.it;
 var expect = Code.expect;
 
 
-describe('Plugin', function () {
+describe('Database root', function () {
     it('should work', function (done) {
         var server = new Hapi.Server();
         var plugin = new Plugin();
@@ -21,6 +21,24 @@ describe('Plugin', function () {
             expect(plugin._register()).to.be.a.string();
             expect(plugin.openDatabase).to.be.a.function();
             expect(plugin.db).to.be.exists();
+            done();
+        });
+    });
+});
+
+describe('Database user', function () {
+    it('should work', function (done) {
+        var server = new Hapi.Server();
+        var plugin = new Plugin();
+        server.connection({host: 'localhost', port: 80});
+
+        server.register(plugin, function (err) {
+            expect(err).to.not.exist();
+            expect(plugin.user.getUserById).to.be.a.function();
+            expect(plugin.user.updateUser).to.be.a.function();
+            expect(plugin.user.createUser).to.be.a.function();
+            expect(plugin.user.getUserLogin).to.be.a.function();
+            expect(plugin.user.getUser).to.be.a.function();
             done();
         });
     });
